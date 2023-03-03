6266103671cfd2ef98882036518df25fa3566602
Maxens Gagne <mgagne@z4r12p3.42lyon.fr>
2023-03-03 16:51:05 +0100

    errors handle

--- fdf_error.c
+++ fdf_error.c
@@ -6,7 +6,7 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:39:53 by mgagne            #+#    #+#             */
-/*   Updated: 2023/01/28 02:56:49 by mgagne           ###   ########.fr       */
+/*   Updated: 2023/03/03 16:50:51 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,35 @@
 
 void	parse_error(void)
 {
-
-	//write(1, "error : parsing or invalid file\n", 32);
-	exit(1);
+	write(2, "error : parsing or invalid file\n", 32);
+	exit(EXIT_FAILURE);
 	return ;
 }
 
 void	malloc_error(void)
 {
-	//write(1, "error : malloc\n", 15);
-	exit(1);
+	write(2, "error : malloc\n", 15);
+	exit(EXIT_FAILURE);
 	return ;
 }
 
 void	nb_arg_error(void)
 {
-	//write(1, "error : program only takes 1 argument\n", 38);
-	exit(1);
+	write(2, "error : program only takes 1 argument\n", 38);
+	exit(EXIT_FAILURE);
 	return ;
 }
 
 void	arg_format_error(void)
 {
-	//write(1, "error : file must be \".fdf\" type\n", 33);
-	exit(1);
+	write(2, "error : file must be \".fdf\" type\n", 33);
+	exit(EXIT_FAILURE);
 	return ;
 }
 
 void	read_error(void)
 {
-	//write(1, "error : couldn't open/read from file\n", 37);
-	exit(1);
+	write(2, "error : couldn't open/read from file\n", 37);
+	exit(EXIT_FAILURE);
 	return ;
 }
